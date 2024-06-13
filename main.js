00a372be47d262a008f315249af88bf6186c10a8
Student <115100395+Asma2528@users.noreply.github.com>
2024-06-13 12:21:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,42 @@
-let menu = document.getElementById("menu");
-let navItems = document.getElementById("navItems");
+document.addEventListener('DOMContentLoaded', (event) => {
+  let menu = document.getElementById("menu");
+  let navItems = document.getElementById("navItems");
+  let cross = document.getElementById("Cross");
+  let slider = document.getElementById("slider");
 
-menu.addEventListener("click", function () {
-  if (
-    navItems.style.display === "none" ||
-    navItems.style.display === ""
-  ) {
-    navItems.style.display = "flex";
-    menu.style.display = "none";
-    Cross.style.display = "inline-block";
-  }
-});
+  const allMicrosoftDropdown = document.getElementById('allMicrosoftDropdown');
+  const dropdownItems = document.getElementById('dropdownItems');
 
-Cross.addEventListener("click", function () {
-  navItems.style.display = "none";
-  Cross.style.display = "none";
-  menu.style.display = "inline-block";
+  menu.addEventListener("click", function () {
+    navItems.classList.toggle("hidden");
+    menu.classList.toggle("hidden");
+    cross.classList.toggle("hidden");
+    slider.classList.toggle("hidden");
+
+  });
+
+  cross.addEventListener("click", function () {
+    navItems.classList.toggle("hidden");
+    cross.classList.toggle("hidden");
+    menu.classList.toggle("hidden");
+    slider.classList.toggle("hidden");
+  });
+
+  allMicrosoftDropdown.addEventListener('click', () => {
+    dropdownItems.classList.toggle('hidden');
+    allMicrosoftDropdown.classList.toggle('bg-gray-100');
+    allMicrosoftDropdown.classList.toggle('border');
+    allMicrosoftDropdown.classList.toggle('border-black');
+    allMicrosoftDropdown.classList.toggle('border-dashed');
+
+  });
+
+  // Close dropdown if clicked outside
+  document.addEventListener('click', (e) => {
+    if (!allMicrosoftDropdown.contains(e.target) && !dropdownItems.contains(e.target)) {
+      dropdownItems.classList.add('hidden');
+      allMicrosoftDropdown.classList.remove('bg-gray-100', 'border', 'border-dashed', 'border-spacing-4');
+    }
+  });
 });
 
